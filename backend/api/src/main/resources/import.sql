c09f8f18418069b14cefbd791f07d044979d2605
hmvir <harmanvir.singh@stud.fh-campuswien.ac.at>
2022-01-21 18:23:59 +0100

    database (application properties), import sql

---
+++ backend/api/src/main/resources/import.sql
@@ -0,0 +1,14 @@
+INSERT INTO `fhoverflow`.`post` (`id`, `content`, `title`, `dtype`, `type`) VALUES (1, 'How was your day?', 'I just wanted to know how your day was', 'QUESTION', 1);
+INSERT INTO `fhoverflow`.`post` (`id`, `content`, `title`, `dtype`, `type`) VALUES (2, 'How was your month?', 'I just wanted to know how your day was', 'QUESTION', 2);
+INSERT INTO `fhoverflow`.`post` (`id`, `content`, `title`, `dtype`, `type`) VALUES (3, 'How was your year?', 'I just wanted to know how your day was', 'QUESTION', 3);
+INSERT INTO `fhoverflow`.`post` (`id`, `content`, `title`, `dtype`, `type`) VALUES (4, 'How was your life?', 'I just wanted to know how your day was', 'QUESTION', 4);
+
+
+INSERT INTO `fhoverflow`.`post` (`id`, `question_id`, `content`, `dtype`, `type`) VALUES (5, 1, 'was pretty good', 'ANSWER', 1);
+INSERT INTO `fhoverflow`.`post` (`id`, `question_id`, `content`, `dtype`, `type`) VALUES (6, 1, 'well', 'ANSWER', 2);
+INSERT INTO `fhoverflow`.`post` (`id`, `question_id`, `content`, `dtype`, `type`) VALUES (7, 2, 'coud be better', 'ANSWER', 3);
+INSERT INTO `fhoverflow`.`post` (`id`, `question_id`, `content`, `dtype`, `type`) VALUES (8, 2, 'top', 'ANSWER', 4);
+INSERT INTO `fhoverflow`.`post` (`id`, `question_id`, `content`, `dtype`, `type`) VALUES (9, 3, 'nice', 'ANSWER', 5);
+INSERT INTO `fhoverflow`.`post` (`id`, `question_id`, `content`, `dtype`, `type`) VALUES (10, 4, 'wym', 'ANSWER', 6);
+
+
